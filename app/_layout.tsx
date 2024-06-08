3002fd420b4699b81333b8d26845f06c224208da
Gianmarco Segura <gseguracordova@gmail.com>
2024-06-08 18:28:25 +0200

    feat: added notLogged and logged routes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,21 +2,38 @@ import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import 'react-native-reanimated';
-
 import { useColorScheme } from '@/hooks/useColorScheme';
 import { PaperProvider } from 'react-native-paper';
+import { onAuthStateChanged } from 'firebase/auth';
+import { router } from 'expo-router';
+import { getApp, getAuth } from '@/utils/firebase';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const [isAdmin, setIsAdmin] = useState(false);
+  const [currentUser, setCurrentUser] = useState<any>(null);
   const colorScheme = useColorScheme();
   const theme = colorScheme === 'dark' ? DarkTheme : DefaultTheme
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
+
+  useEffect(() => {
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user);
+      if(user) {
+        console.log('user: ', user);
+      } else {
+        console.log('NoUser: ', user);
+        router.replace('(notLogged)')
+      }
+    })
+  }, [])
 
   useEffect(() => {
     if (loaded) {
@@ -32,10 +49,12 @@ export default function RootLayout() {
     <ThemeProvider value={theme}>
       <PaperProvider theme={theme}>
         <Stack>
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="(notLogged)" options={{ headerShown: true }} />
+          <Stack.Screen name="(logged)" options={{ headerShown: true }} />
           <Stack.Screen name="+not-found" />
         </Stack>
       </PaperProvider>
     </ThemeProvider>
   );
 }
+
---
+++ utils/firebase.ts
@@ -0,0 +1,23 @@
+// ./services/firebase.js
+import { initializeApp, getApp } from "firebase/app";
+import { initializeAuth, getAuth, getReactNativePersistence } from 'firebase/auth';
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBBUo9uBaV61xzzzkeIUsUo9mvrmcm8wuI",
+  authDomain: "abailar-6017e.firebaseapp.com",
+  projectId: "abailar-6017e",
+  storageBucket: "abailar-6017e.appspot.com",
+  messagingSenderId: "473452662614",
+  appId: "1:473452662614:web:06042f08f83597a132d0f7",
+  measurementId: "G-B28M428GHQ"
+};
+
+// initialize Firebase App
+const app = initializeApp(firebaseConfig);
+// initialize Firebase Auth for that app immediately
+const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage)
+});
+
+export { app, auth, getApp, getAuth };
